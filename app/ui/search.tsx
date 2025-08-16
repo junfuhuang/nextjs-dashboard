6808fb7c8e98c677a7756afb633104d29f1fbfb3
junfuhuang <junfuhuang@tencent.com>
2025-08-16 17:43:06 +0800

    fix: 添加测试代码

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,17 +1,55 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
+// 传入一个 plcaeholder 属性，用来设置输入框占位文本
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  // 输入框内容变化时被调用，把输入的内容打印出来
+  // function handleSearch(term: string) {
+  //   console.log(`Searching... ${term}`);
+
+  //   const params = new URLSearchParams(searchParams);
+  //   if (term) {
+  //     params.set('query', term);
+  //   } else {
+  //     params.delete('query');
+  //   }
+  //   replace(`${pathname}?${params.toString()}`);
+  // }
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
+   
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
+      {/*label: 用于无障碍，className="sr-only"代表屏幕阅读器可见但页面不显示*/}
       <label htmlFor="search" className="sr-only">
         Search
       </label>
+      {/* 输入内容变化时调用 handleSearch */}
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
+      {/* 放大镜图标 */}
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
   );
